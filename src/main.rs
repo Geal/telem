44361296ce20afb505a491639b7e60adf5f6712e
Gary Pennington <gary@apollographql.com>
2022-01-20 15:47:03 +0000

    Minimal example for geoffroy
    Just loops and repeatedly sets the tracer.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use crate::stdout::*;
+use opentelemetry::sdk::export::trace::stdout;
+use opentelemetry::trace::Tracer;
+use tracing::{instrument, span};
+
+#[instrument(name = "do nothing", level = "error")]
+async fn do_nothing(doing: &str) {
+    tracing::info!("huh");
+    eprintln!("doing {}", doing);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::fmt().json().init();
+
+    loop {
+        let tracer = new_pipeline().with_pretty_print(true).install_simple();
+
+        let root = span!(tracing::Level::INFO, "app_start", work_units = 2);
+        let _entry = root.enter();
+
+        tracer
+            .in_span("doing_work", |_cx| async {
+                // Traced app logic here...
+                do_nothing("with context").await
+            })
+            .await;
+
+        tracing::info!("hmm");
+        do_nothing("with it").await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+    }
+    // Shutdown trace pipeline
+    // Ok(global::shutdown_tracer_provider())
+}
